4674d6d58878006000c775c3574f539a64c252d5
aldoresendiz <aldoresendiz@gmail.com>
2023-11-09 20:07:39 -0600

    API that retrieves several languages

--- server.js
+++ server.js
@@ -10,13 +10,20 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 
+
+
 // MIDDLEWARE
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+
+// Languages
+const languagesController = require('./controllers/languages_controller.js')
+app.use('/languages', languagesController)
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to the Hello World! API')
 })
+
 
 // LISTEN
 app.listen(PORT, () => {
